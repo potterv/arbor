65ef1fae80e844bb85ac7eb175d839eb6f9639fe
potterv <gonchar.vladimir@hotmail.com>
2018-08-28 13:00:06 +0300

    add login form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from flask import render_template, flash, redirect
 from arbor.app import app
-#from arbor.app.forms import LoginForm
+from arbor.app.AuthForm import LoginForm
 #@app.route('/')
 @app.route('/arbor')
 def arbor():
    # print(app.config['SECRET_KEY'])
    return render_template('arbor.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
