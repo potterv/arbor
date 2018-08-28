f7949268fda47d7a871000a7b163d88ba5a960d4
potterv <gonchar.vladimir@hotmail.com>
2018-08-28 11:04:38 +0300

    edit web

--- app/views.py
+++ app/views.py
@@ -1,10 +1,7 @@
 from flask import render_template, flash, redirect
 from arbor.app import app
 #from arbor.app.forms import LoginForm
-@app.route('/index')
-def index():
-   return 'hello'
-
+#@app.route('/')
 @app.route('/arbor')
 def arbor():
    # print(app.config['SECRET_KEY'])
