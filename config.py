24dd9fede9db3ca9525a4e080f793566f084e1b5
potterv <gonchar.vladimir@hotmail.com>
2018-09-20 16:22:03 +0300

    update config

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     ''' 
         SQLALCHEMY_DATABASE_URI необходим для расширения Flask-SQLAlchemy. Это путь к файлу с нашей базой данных.
-        SQLALCHEMY_MIGRATE_REPO — это папка, где мы будем хранить файлы SQLAlchemy-migrate.
+        
     '''
-
+    # SQLALCHEMY_DATABASE_URI необходим для расширения Flask-SQLAlchemy. Это путь к файлу с нашей базой данных.
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_MIGRATE_REPO — это папка, где мы будем хранить файлы SQLAlchemy-migrate.
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    hostname=os.environ.get('hostname') or "0.0.0.0"
+    host=os.environ.get('host') or "0.0.0.0"
